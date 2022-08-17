00689521775e286d6cf876d4505e3c9d75e6fddb
Kyle <rabidbusiness@gmail.com>
2022-08-17 16:01:36 +1000

    updated dates and added userId to query

--- pharmacyApp/src/components/sign-in/SignInForm.js
+++ pharmacyApp/src/components/sign-in/SignInForm.js
@@ -33,6 +33,7 @@ function SignInForm(props) {
             access_token: data.access_token,
             username: data.username,
           });
+          localStorage.setItem("userId", data.user_id) /////
           navigate("/" + data.username);
         }
       });
