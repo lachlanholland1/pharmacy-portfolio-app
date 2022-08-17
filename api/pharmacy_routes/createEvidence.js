72d8124d11f791fcab5e29f91a21fa7266569bc6
Kyle <rabidbusiness@gmail.com>
2022-08-17 16:01:36 +1000

    updated dates and added userId to query

--- api/pharmacy_routes/createEvidence.js
+++ api/pharmacy_routes/createEvidence.js
@@ -3,19 +3,18 @@ const router = express.Router();
 const db = require("../connection.js");
 
 router.post("/", (req, res, next) => {
-  const idevidenceitems = Math.random().toString(16).slice(2);
+  var date = new Date();
   const title = req.body.title;
-  const userId = Math.random().toString(16).slice(2);
+  const userId = req.body.userId;
   const description = req.body.description;
   const impactstatement = req.body.impactstatement;
-//   const uploaddate = req.body.uploaddate;
-  const uploaddate = Date.now();
+  const uploaddate = (date.toISOString().slice(0, 19).replace('T', ' ')).toString();
   const procurementdate = req.body.date;
   const attachment = "to do";
   console.log(req.body);
   db.query(
-    "INSERT INTO evidenceitems (idevidenceitems, users_id, title, description, impactstatement, uploaddate, procurementdate, attachment) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-    [idevidenceitems, userId, title, description, impactstatement, uploaddate, procurementdate, attachment],
+    "INSERT INTO evidenceitems (users_id, title, description, impactstatement, uploaddate, procurementdate, attachment) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    [userId, title, description, impactstatement, uploaddate, procurementdate, attachment],
     (err, result) => {
       res.sendStatus(200);
     }
