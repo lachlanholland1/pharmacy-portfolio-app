e5f748e9b27754448855fa5cf5c7b7605dd086dd
klavery <kylelavery@outlook.com>
2022-09-19 19:21:22 +1000

    got jest test framework working

---
+++ api/src/viewEvidence.test.js
@@ -0,0 +1,8 @@
+let viewEvidence = require("../pharmacy_routes/ViewEvidence");
+const express = require("express");
+const router = express.Router();
+
+
+it("Testing to see if Jest works", () => {
+  expect(1).toBe(1);
+});
