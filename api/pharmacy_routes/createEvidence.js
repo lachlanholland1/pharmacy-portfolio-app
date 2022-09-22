687656716167a41cbcb22b2817c79431f52b711c
Lachlan Holland <lachholland@gmail.com>
2022-09-23 01:27:41 +1000

    sessions finished needs review

--- api/pharmacy_routes/createEvidence.js
+++ api/pharmacy_routes/createEvidence.js
@@ -5,21 +5,34 @@ const db = require("../connection.js");
 router.post("/", (req, res, next) => {
   var date = new Date();
   const title = req.body.title;
-  const userId = parseInt(req.body.userId);
+  const userId = req.session.userid;
   const description = req.body.description;
   const impactstatement = req.body.impactstatement;
-  const uploaddate = (date.toISOString().slice(0, 19).replace('T', ' ')).toString();
+  const uploaddate = date
+    .toISOString()
+    .slice(0, 19)
+    .replace("T", " ")
+    .toString();
   const procurementdate = req.body.date;
   const attachment = req.body.attachment;
   console.log(req.body);
   db.query(
     "INSERT INTO evidenceitems (users_id, title, description, impactstatement, uploaddate, procurementdate, attachment) VALUES (?, ?, ?, ?, ?, ?, ?)",
-    [userId, title, description, impactstatement, uploaddate, procurementdate, attachment],
+    [
+      userId,
+      title,
+      description,
+      impactstatement,
+      uploaddate,
+      procurementdate,
+      attachment,
+    ],
     (err, result) => {
       if (err) {
         console.log(err);
         res.sendStatus(401);
-        return;}
+        return;
+      }
       res.sendStatus(200);
     }
   );
