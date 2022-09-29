a81391e73437140d3dc1b6ebd85cff323770931a
klavery <kylelavery@outlook.com>
2022-09-29 17:36:20 +1000

    added separate view peer review page

--- api/pharmacy_routes/getAllUsers.js
+++ api/pharmacy_routes/getAllUsers.js
@@ -4,8 +4,6 @@ const db = require("../connection.js");
 
 router.post("/", (req, res, next) => {
   db.query("SELECT user_id, firstname, surname FROM Users", (err, result) => {
-    //   const response = result;
-    //   console.log(response.length);
     console.log(result);
     res.send(result);
   });
---
+++ pharmacyApp/src/components/view-review/ViewPeerReview.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState, useReducer } from "react";
+import { Controller, useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate, useSearchParams, Link } from "react-router-dom";
+import style from "./style.css";
+import ViewPeerReviewForm from "./ViewPeerReviewForm";
+
+function ViewPeerReview(props) {
+  const { auth } = useAuth();
+  const [evidenceCriteria, setEvidenceCriteria] = useState(null);
+  localStorage.removeItem("currentDomain");
+
+  useEffect(() => {
+    const request = {
+      access_token: auth.access_token,
+      username: auth.username,
+    };
+    fetch("/api/evidence-criteria", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setEvidenceCriteria(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {evidenceCriteria ? (
+        <ViewPeerReviewForm evidenceCriteria={evidenceCriteria} />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
+
+export default ViewPeerReview;
