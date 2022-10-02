62c49407d8cf1941656398b8e965049c4b6a7163
Michael McGowan <michael.mcgowan@isgq.com.au>
2022-10-02 17:04:23 +1000

    Domains, Standards, Competencies (Create & View)

---
+++ api/pharmacy_routes/createCompetency.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../connection.js");
+
+router.post("/", (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const standard = req.body.standards_id;
+  const status = req.body.status;
+  console.log(req.body);
+  db.query(
+    "INSERT INTO competencies (title, description, standards_id, status) VALUES (?, ?, ?, ?)",
+    [title, description, standard, status],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(401);
+        return;}
+      res.sendStatus(200);
+    }
+  );
+});
+
+module.exports = router;
---
+++ api/pharmacy_routes/createStandard.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../connection.js");
+
+router.post("/", (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const domain = req.body.domains_id;
+  const status = req.body.status;
+  console.log(req.body);
+  db.query(
+    "INSERT INTO standards (title, description, domains_id, status) VALUES (?, ?, ?, ?)",
+    [title, description, domain, status],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(401);
+        return;}
+      res.sendStatus(200);
+    }
+  );
+});
+
+module.exports = router;
