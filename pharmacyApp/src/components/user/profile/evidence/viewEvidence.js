f0f1dd1fc56af9658285dd7253cdb1d3d79dd933
Lachlan Holland <lachholland@gmail.com>
2022-10-06 14:35:22 +1000

    dsadsa

--- pharmacyApp/src/components/user/profile/evidence/viewEvidence.js
+++ pharmacyApp/src/components/user/profile/evidence/viewEvidence.js
@@ -13,15 +13,13 @@ export default function ViewEvidence(props) {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
   const [evidenceData, setEvidenceData] = useState([]);
-  const [evidenceReviews, setEvidenceReviews] = useState([]);
+  const [evidenceReviews, setEvidenceReviews] = useState(null);
   const { auth, setAuth } = useAuth();
+  const [loaded, setLoaded] = useState(false);
 
   let viewingProfile = localStorage.getItem("profile");
   localStorage.setItem("evidence_id", evidenceData.idevidenceitems);
   localStorage.setItem("attachment", evidenceData.attachment);
-
-  console.log(auth.user_id);
-  console.log(evidenceData.users_id);
 
   useEffect(() => {
     const request = { idevidenceitems: id };
@@ -34,54 +32,61 @@ export default function ViewEvidence(props) {
       .then((details) => {
         setEvidenceData(details.evidence_data);
         setEvidenceReviews(details.evidence_reviews);
-      });
+      })
+      .then(() => setLoaded(true));
   }, []);
   return (
     <div>
-      <div className={style.container}>
-        <div className={style.sign}>
-          <h1> {evidenceData.title} </h1>
-          <p>
-            Date Created:{" "}
-            {Moment(evidenceData.procurementdate, "YYYY-MM-DD").format(
-              "DD/MM/YYYY"
+      {loaded ? (
+        <div className={style.container}>
+          <div className={style.sign}>
+            <h1> {evidenceData.title} </h1>
+            <p>
+              Date Created:{" "}
+              {Moment(evidenceData.procurementdate, "YYYY-MM-DD").format(
+                "DD/MM/YYYY"
+              )}
+            </p>
+            <p>
+              Date Uploaded:{" "}
+              {Moment(evidenceData.uploaddate, "YYYY-MM-DD").format(
+                "DD/MM/YYYY"
+              )}
+            </p>
+            <p> Description: {evidenceData.description}</p>
+            <p> Impact Statement: {evidenceData.impactstatement}</p>
+            {/* <p> Attachment: {evidenceData.attachment}</p> */}
+            <button
+              className={style.myButton}
+              onClick={() => DownloadImageToS3(evidenceData.attachment)}
+            >
+              View Evidence
+            </button>
+            <br />
+            <br />
+            <Flagged id={evidenceData.users_id} />
+            <br />
+            <br />
+            {auth.user_id === evidenceData.users_id && !evidenceReviews ? (
+              <Link to={`/create-self-review/?id=${id}`}>
+                <button className={style.myButton}>Self Review</button>
+              </Link>
+            ) : (
+              <></>
             )}
-          </p>
-          <p>
-            Date Uploaded:{" "}
-            {Moment(evidenceData.uploaddate, "YYYY-MM-DD").format("DD/MM/YYYY")}
-          </p>
-          <p> Description: {evidenceData.description}</p>
-          <p> Impact Statement: {evidenceData.impactstatement}</p>
-          {/* <p> Attachment: {evidenceData.attachment}</p> */}
-          <button
-            className={style.myButton}
-            onClick={() => DownloadImageToS3(evidenceData.attachment)}
-          >
-            View Evidence
-          </button>
-          <br />
-          <br />
-          <Flagged id={evidenceData.users_id} />
-          <br />
-          <br />
-          {auth.user_id === evidenceData.users_id ? (
-            <Link to={`/create-self-review/?id=${id}`}>
-              <button className={style.myButton}>Self Review</button>
+            <br />
+            <br />
+            <Link to={"/" + viewingProfile}>
+              <button className={style.myButton}>Back</button>
             </Link>
-          ) : (
-            <></>
-          )}
-          <br />
-          <br />
-          <Link to={"/" + viewingProfile}>
-            <button className={style.myButton}>Back</button>
-          </Link>
-          <h3>Self Reviews</h3>
-          <EvidenceReviews reviews={evidenceReviews} />
-          <PeerReviewTable />
+            <h3>Self Reviews</h3>
+            <EvidenceReviews reviews={evidenceReviews} />
+            <PeerReviewTable />
+          </div>
         </div>
-      </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
