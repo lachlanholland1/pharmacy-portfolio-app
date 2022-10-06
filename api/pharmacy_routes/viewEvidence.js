ebb6bff65bc8f259642704671159668924ba4217
Lachlan Holland <lachholland@gmail.com>
2022-10-06 14:35:22 +1000

    dsadsa

--- api/pharmacy_routes/viewEvidence.js
+++ api/pharmacy_routes/viewEvidence.js
@@ -4,33 +4,44 @@ const db = require("../connection.js");
 
 router.post("/", (req, res, next) => {
   const idevidenceitems = req.body.idevidenceitems;
-  db.query(
-    "SELECT * FROM evidenceitems WHERE idevidenceitems = ?",
-    [idevidenceitems],
-    (err, result) => {
-      const response = { evidence_data: result[0] };
-      //   if (err) {
-      //     console.log(err);
-      //     res.sendStatus(401);
-      //     return;}
-      // //   res.sendStatus(200);
-      db.query(
-        "SELECT * FROM evidencereviews INNER JOIN Users ON evidencereviews.reviewers_id = Users.user_id WHERE evidenceitems_id = ? GROUP BY idevidencereview",
-        [idevidenceitems],
-        (err, result) => {
-          //   if (err) {
-          //     console.log(err);
-          //     res.sendStatus(401);
-          //     return;}
-          // //   res.sendStatus(200);
-          response.evidence_reviews = result;
-          console.log("response:");
-          console.log(response);
-          res.send(response);
+  const response = { evidence_data: {} };
+
+  const evidenceData = new Promise((resolve, reject) => {
+    db.query(
+      "SELECT * FROM evidenceitems WHERE idevidenceitems = ?",
+      [idevidenceitems],
+      (err, result) => {
+        if (err) {
+          return reject(err);
         }
-      );
-    }
-  );
+        if (!result.length) {
+          return reject(Promise.reject());
+        }
+        response.evidence_data = result[0];
+        return resolve();
+      }
+    );
+  });
+
+  const selfReviewEnabled = new Promise((resolve, reject) => {
+    db.query(
+      "SELECT * FROM evidencereviews INNER JOIN Users ON evidencereviews.reviewers_id = Users.user_id WHERE evidenceitems_id = ? GROUP BY idevidencereview",
+      [idevidenceitems],
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        response.evidence_reviews = result[0];
+        return resolve();
+      }
+    );
+  });
+
+  Promise.all([evidenceData, selfReviewEnabled])
+    .then(() => {
+      res.send(response);
+    })
+    .catch((err) => res.sendStatus(500));
 });
 
 module.exports = router;
