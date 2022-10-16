709e8e733701f26ad056857e7ebe11976230f2c4
klavery <kylelavery@outlook.com>
2022-10-16 20:39:38 +1100

    delete evidence fixed

---
+++ pharmacyApp/src/components/editEvidence/DeleteEvidence.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import useAuth from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
+import style from "./style.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+function DeleteEvidence(props) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const [evidenceData, setEvidenceData] = useState({}); //changed from ([]);
+  const { auth, setAuth } = useAuth();
+
+  //   const [userData, setUserData] = useState({});
+
+  function submit() {
+    fetch("/api/deleteevidence", {
+      method: "POST",
+      body: JSON.stringify({
+        idevidenceitems: id,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then(navigate("/" + auth.username));
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.sign}>
+        <br />
+
+        <h2 className={style.center}>
+          Are you sure you want to delete this evidence?
+        </h2>
+        <br />
+        <div className={style.buttonSpace}>
+          <button className={style.myButton2} onClick={submit}>
+            Yes
+          </button>
+          <Link to={`/evidence?id=${id}`}>
+            <button className={style.myButton2}>No</button>
+          </Link>
+        </div>
+        <br />
+        <br />
+        <div className={style.buttonSpace}>
+          <Link to={`/evidence?id=${id}`}>
+            <button className={style.myButton2}>Back</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteEvidence;
