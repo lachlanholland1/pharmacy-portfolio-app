ce6bed6186ea51c0396b6d9064ec3471a77ad654
Lachlan Holland <lachholland@gmail.com>
2022-10-28 03:51:37 +1000

    bad tests

--- pharmacyApp/src/tests/index.test.js
+++ pharmacyApp/src/tests/index.test.js
@@ -2,61 +2,198 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { screen, render, cleanup, fireEvent } from "@testing-library/react";
 import DomainsTable from "../components/domains/view-domains/DomainsTable";
-import StandardsTable from "../components/standards/view-standards/StandardsTable";
 import mockData from "./mockData";
-// 9. ADD YOUR REACT ELEMENT YOUR TESTING
+
 const routerContainer = { wrapper: BrowserRouter };
 const router1Container = { wrapper: BrowserRouter };
 
-// 3. COPY FROM HERE...
-//5. RENAME YOUR TEST NAME HERE
+// JEST FRONT-END TESTING
+
 describe("DomainsTable Component", () => {
   jest.restoreAllMocks();
 
   global.fetch = jest.fn().mockImplementation(() =>
     Promise.resolve({
       status: 400,
-      json: () => Promise.resolve(mockData.domains_table), // 6. CHANGE TO BE NEW MOCK DATA NAME
+      json: () => Promise.resolve(mockData.domains_table),
     })
   );
   beforeAll(() => {});
   afterAll(cleanup);
   test("should contain word Domain", async () => {
-    // 7. CHANGE DESCRIPTION
-    render(<DomainsTable />, routerContainer); // 8. CHANGE REACT ELEMENT YOU ARE TESTING. SEE 9 ABOVE ^
-    const message = "Domains"; // 10. CHANGE HOW YOU TEST REACT ELEMENTS LOADED. THIS JUST TESTS FOR WORD "Domains"
+    render(<DomainsTable />, routerContainer);
+    const message = "Domains";
 
-    expect(await screen.findByText(message)).toBeInTheDocument(); // 11. THIS AWAITS UNTIL PAGE LOADED AND CHECKS IF FOUND
+    expect(await screen.findByText(message)).toBeInTheDocument();
   });
 });
 
 describe("StandardsTable Component", () => {
-    jest.restoreAllMocks();
-  
-    global.fetch = jest.fn().mockImplementation(() =>
-      Promise.resolve({
-        status: 400,
-        json: () => Promise.resolve(mockData.Standards_table), // 6. CHANGE TO BE NEW MOCK DATA NAME
-      })
-    );
-    beforeAll(() => {});
-    afterAll(cleanup);
-    test("should contain word Standard", async () => {
-      // 7. CHANGE DESCRIPTION
-      render(<StandardsTable />, router1Container); // 8. CHANGE REACT ELEMENT YOU ARE TESTING. SEE 9 ABOVE ^
-      const message = "Standards"; // 10. CHANGE HOW YOU TEST REACT ELEMENTS LOADED. THIS JUST TESTS FOR WORD "Standards"
-  
-      expect(await screen.findByText(message)).toBeInTheDocument(); // 11. THIS AWAITS UNTIL PAGE LOADED AND CHECKS IF FOUND
-    });
+  jest.restoreAllMocks();
+
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Standard", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "Standards";
+
+    expect(await screen.findByText(message)).toBeInTheDocument();
   });
+});
 
+describe("Competencies Component", () => {
+  jest.restoreAllMocks();
 
-// 3. cont. ...TO HERE
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Competencies", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "Competencies";
 
-// 12. !!!!TO RUN: IN PHARMACY APP FOLDER IN COMMAND LINE TYPE COMMAND: npm test
+    expect(await screen.findByText(message)).toBeInTheDocument();
+  });
+});
 
-//13. FINAL NOTE: You may run into issues depending on the component and what it requires. 
-//Try and google the fix. 
-//If get stuck I can help. 
-//For simple tests to begin with you can just test that a word is rendered in the component like I have with "Domains"
+describe("Reviewers Card Component", () => {
+  jest.restoreAllMocks();
 
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Reviewers", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "Reviewers";
+
+    expect(await screen.findByText(message)).toBeInTheDocument();
+  });
+});
+
+describe("Login Form Component", () => {
+  jest.restoreAllMocks();
+
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Login", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "Login";
+
+    expect(await screen.findByText(message)).toBeInTheDocument();
+  });
+});
+
+describe("Users Table Component", () => {
+  jest.restoreAllMocks();
+
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Users", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "Users";
+
+    expect(await screen.findByText(message)).toBeInTheDocument();
+  });
+});
+
+describe("Menu Navbar Component", () => {
+  jest.restoreAllMocks();
+
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Navbar", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "";
+
+    expect(await screen.findByText(message)).toBeInTheDocument();
+  });
+});
+
+describe("Sign Up Form Component", () => {
+  jest.restoreAllMocks();
+
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Sign Up Form", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "Sign Up";
+
+    expect(await screen.findByText(message)).toBeInTheDocument();
+  });
+});
+
+describe("Admin Table Component", () => {
+  jest.restoreAllMocks();
+
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Admins", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "Admins";
+
+    expect(await screen.findByText(message)).toBeInTheDocument();
+  });
+});
+
+describe("Reviewers Table Component", () => {
+  jest.restoreAllMocks();
+  global.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(mockData.Standards_table),
+    })
+  );
+  beforeAll(() => {});
+  afterAll(cleanup);
+  test("should contain word Reviewers", async () => {
+    render(<StandardsTable />, router1Container);
+    const message = "Reviewers";
+
+    expect(await screen.findByText(message)).toBeInTheDocument();
+  });
+});
